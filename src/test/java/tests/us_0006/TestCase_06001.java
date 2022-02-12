5fd3d36ae4439d6f91f29977b7220c898dde53a4
Kubra <kubrapolat1912@gmail.com>
2022-02-12 19:25:09 +0300

    first commit

---
+++ src/test/java/tests/us_0006/TestCase_06001.java
@@ -0,0 +1,11 @@
+package tests.us_0006;
+
+import org.testng.annotations.Test;
+
+public class TestCase_06001 {
+    @Test
+    public void test01(){
+        System.out.println("kübra");
+    }
+
+}
