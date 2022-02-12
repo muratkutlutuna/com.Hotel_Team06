a8faccf15254b8656082c9d1e5eed9e33bc7b6ad
Rana <ranaterzioglu@gmail.com>
2022-02-12 17:42:45 -0500

    first commit

--- src/test/java/tests/us_0010/TestCase_10001.java
+++ src/test/java/tests/us_0010/TestCase_10001.java
@@ -1,5 +1,40 @@
 package tests.us_0010;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class TestCase_10001 {
+
+    WebDriver driver;
+
+    @BeforeClass
+
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+    }
+
+    @Test
+
+   public  void roomsTest(){
+       //1- "https://www.hotelmycamp.com/" sayfasina gider
+        driver.get("https://www.hotelmycamp.com/");
+
+        //"Rooms" secenegine tiklar
+        driver.findElement(By.xpath("//a[text()='Rooms']")).click();
+
+        //ilk cikan oda da "Book Now" butonuna tiklar
+        driver.findElement(By.xpath("//a[@class='btn-custom']")).click();
+
+    }
 
 }
