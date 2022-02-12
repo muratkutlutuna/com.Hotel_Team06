729941dbf5c0378e83fca8c58e23c3b322a6b13b
Murat Kutlu Tuna <muratkutlutuna@gmail.com>
2022-02-12 16:55:10 +0100

    deneme iki

--- src/test/java/tests/us_0001/TestCase_01001.java
+++ src/test/java/tests/us_0001/TestCase_01001.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class TestCase_01001 {
     @Test
     public void test_01(){
-        System.out.println("kutlu");
+        System.out.println("kutlu"+"tuna");
     }
 }
