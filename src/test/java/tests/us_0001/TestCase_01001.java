c1953fa73c8f4b8910533b7a4b31154f7b223d34
htctskn <94467689+htctskn@users.noreply.github.com>
2022-02-12 20:07:15 +0300

    deneme

--- src/test/java/tests/us_0001/TestCase_01001.java
+++ src/test/java/tests/us_0001/TestCase_01001.java
@@ -1,4 +1,10 @@
 package tests.us_0001;
 
+import org.testng.annotations.Test;
+
 public class TestCase_01001 {
+    @Test
+    public void test01(){
+        System.out.println("hatice");
+    }
 }
