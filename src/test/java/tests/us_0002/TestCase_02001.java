f12d1cc55325c9905d52a162f45d4345e75b69c9
Erkan <erkndmrbs52@gmail.com>
2022-02-12 20:06:25 +0300

    deneme

--- src/test/java/tests/us_0002/TestCase_02001.java
+++ src/test/java/tests/us_0002/TestCase_02001.java
@@ -1,4 +1,15 @@
 package tests.us_0002;
 
+
+import org.testng.annotations.Test;
+
 public class TestCase_02001 {
+   @Test
+   public void test01(){
+
+       System.out.println(" mirza batu demirbas");
+
+
+   }
+
 }
