fc34c18e0f5d8142a2aa7186f33f49fc1ad19fb5
Feyzanur Tetik <feyzanurtetik@gmail.com>
2022-02-13 16:10:16 +0100

    first commit

--- src/test/java/tests/us_0003/TestCase_03002.java
+++ src/test/java/tests/us_0003/TestCase_03002.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class TestCase_03002 {
     @Test
     public void test01(){
-        System.out.println("feyza");
+        System.out.println("feyzaa");
     }
 }
