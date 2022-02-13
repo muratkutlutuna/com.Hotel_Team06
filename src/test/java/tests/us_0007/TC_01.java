71a02be36f06584b8d3fc0fea335398337cfb1f7
mehmet surucu <mehmetsmarket@gmail.com>
2022-02-13 12:56:50 +0300

    mehmet_first commit

---
+++ src/test/java/tests/us_0007/TC_01.java
@@ -0,0 +1,20 @@
+package tests.us_0007;
+
+
+public class TC_01 {
+
+    //HOTELMYCAMP ana sayfasına gidilir
+    //Login butonuna tiklanir
+    //Username'e gecerli bir username girilir
+    //Password'a gecerli bir password girilir
+    //Log in butonuna tiklanir
+    //"ListOfUsers" yazisinin gorunurlugu test edilir
+
+
+    @Test
+    public static void firstPush () {
+
+    }
+
+    }
+
