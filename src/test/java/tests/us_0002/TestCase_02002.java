e99d4b51f3b08517025baf7dbe089d5d717f687d
Erkan <erkndmrbs52@gmail.com>
2022-02-13 19:11:23 +0300

    demirbas butu

--- src/test/java/tests/us_0002/TestCase_02002.java
+++ src/test/java/tests/us_0002/TestCase_02002.java
@@ -1,4 +1,12 @@
 package tests.us_0002;
 
+import org.testng.annotations.Test;
+
 public class TestCase_02002 {
+
+    @Test
+    public void test(){
+
+        System.out.println("merhaba");
+    }
 }
