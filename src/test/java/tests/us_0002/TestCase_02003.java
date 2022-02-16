9359bfe825570a8a739af0583644806463803f90
Erkan <erkndmrbs52@gmail.com>
2022-02-16 18:36:32 +0300

    16.02.2022

--- src/test/java/tests/us_0002/TestCase_02003.java
+++ src/test/java/tests/us_0002/TestCase_02003.java
@@ -5,32 +5,40 @@ import org.testng.annotations.Test;
 import pages.HotelMyCampPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.TestBaseRapor;
 
-public class TestCase_02003 {
+public class TestCase_02003 extends TestBaseRapor {
     @Test
     public void passwordTextboxTesti() {
 
         // https://hotelmycamp.com/ sayfasına gidin
         Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        extentTest=extentReports.createTest("HotelMyCamp us_0002  TestCase_02003","https://hotelmycamp.com test edildi");
 
         //"Log in" linki görünülüğünü kontrol edin
-        HotelMyCampPage hotelMyCampPage = new HotelMyCampPage();
-        Assert.assertTrue(hotelMyCampPage.ilkLoginLinki.isDisplayed(), " ilk Login görülemiyor");
+        HotelMyCampPage hotelMyCampPage=new HotelMyCampPage();
+        Assert.assertTrue( hotelMyCampPage.ilkLoginLinki.isDisplayed()," ilk Login görülemiyor");
+        extentTest.info("Log in linki görünülüğünü kontrol edildi");
 
         //"Log in" linkine tıklayın
         hotelMyCampPage.ilkLoginLinki.click();
+        extentTest.info("Log in linkine tıklandi");
 
         //Username textbox görünürlüğünü kontrol edin
-        Assert.assertTrue(hotelMyCampPage.usernameBox.isDisplayed(), "Username textbox görülemiyor");
+        Assert.assertTrue(hotelMyCampPage.usernameBox.isDisplayed(),"Username textbox görülemiyor");
+        extentTest.info("Username textbox görünülüğünü kontrol edildi");
 
         //Username textbox'a Admin username girilmeli
-        hotelMyCampPage.usernameBox.sendKeys("manager"); // username: manager
+        hotelMyCampPage.usernameBox.sendKeys( "manager"); // username: manager
+        extentTest.info("Username textbox'a Admin username girildi");
 
         //Password textbox kontrol edin
        Assert.assertTrue(hotelMyCampPage.passwordBox.isDisplayed()," Password textbox görülemiyor ");
+        extentTest.info("Password textbox kontrol edildi");
 
         //Password textbox'a Admin password girilmeli  Password  : Manager1!
         hotelMyCampPage.passwordBox.sendKeys("Manager1! ");
+        extentTest.info("Password textbox kontrol edildi ");
 
         Driver.closeDriver();
 
--- src/test/java/tests/us_0002/TestCase_02004.java
+++ src/test/java/tests/us_0002/TestCase_02004.java
@@ -6,41 +6,50 @@ import org.testng.asserts.SoftAssert;
 import pages.HotelMyCampPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.TestBaseRapor;
 
-public class TestCase_02004 {
+public class TestCase_02004 extends TestBaseRapor {
     @Test
     public void loginButonunun() {
 
         // https://hotelmycamp.com/ sayfasına gidin
         Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        extentTest=extentReports.createTest("HotelMyCamp us_0002  TestCase_02004","https://hotelmycamp.com test edildi");
 
         //"Log in" linki görünülüğünü kontrol edin
-        HotelMyCampPage hotelMyCampPage = new HotelMyCampPage();
-        SoftAssert softAssert=new SoftAssert();
-        softAssert.assertTrue(hotelMyCampPage.ilkLoginLinki.isDisplayed(), " ilk Login görülemiyor");
+        HotelMyCampPage hotelMyCampPage=new HotelMyCampPage();
+        Assert.assertTrue( hotelMyCampPage.ilkLoginLinki.isDisplayed()," ilk Login görülemiyor");
+        extentTest.info("Log in linki görünülüğünü kontrol edildi");
 
         //"Log in" linkine tıklayın
         hotelMyCampPage.ilkLoginLinki.click();
+        extentTest.info("Log in linkine tıklandi");
 
         //Username textbox görünürlüğünü kontrol edin
-        softAssert.assertTrue(hotelMyCampPage.usernameBox.isDisplayed(), "Username textbox görülemiyor");
+        Assert.assertTrue(hotelMyCampPage.usernameBox.isDisplayed(),"Username textbox görülemiyor");
+        extentTest.info("Username textbox görünülüğünü kontrol edildi");
 
         //Username textbox'a Admin username girilmeli
-        hotelMyCampPage.usernameBox.sendKeys("manager"); // username: manager
+        hotelMyCampPage.usernameBox.sendKeys( "manager"); // username: manager
+        extentTest.info("Username textbox'a Admin username girildi");
 
         //Password textbox kontrol edin
-        softAssert.assertTrue(hotelMyCampPage.passwordBox.isDisplayed(), " Password textbox görülemiyor ");
+        Assert.assertTrue(hotelMyCampPage.passwordBox.isDisplayed()," Password textbox görülemiyor ");
+        extentTest.info("Password textbox kontrol edildi");
 
         //Password textbox'a Admin password girilmeli  Password  : Manager1!
         hotelMyCampPage.passwordBox.sendKeys("Manager1! ");
+        extentTest.info("Password textbox kontrol edildi ");
 
         //Login butonunun görünürlüğünü kontrol edin
-        softAssert.assertTrue(hotelMyCampPage.loginButonu.isDisplayed()," Login butonunun görülemiyor ");
+        Assert.assertTrue(hotelMyCampPage.loginButonu.isDisplayed()," Login butonunun görülemiyor ");
+        extentTest.info("Login butonunun görünülüğünü kontrol edildi");
 
         //Login butonuna tıklayın
         hotelMyCampPage.loginButonu.click();
+        extentTest.info("Log in butonuna tıklandi");
 
-        softAssert.assertAll();
+
         Driver.closeDriver();
           }
        }
